7dd47064aec2b365f27f55e7943efd77877b9f55
wengever <114942469+wengever@users.noreply.github.com>
2025-06-26 23:55:01 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,13 +4,12 @@ from selenium.webdriver.chrome.options import Options
 
 def use_disable_chrome_annoyings():
     options = Options()
-    options.add_argument('--headless')  # 無視覺介面，才能跑在 Docker 裡
-    options.add_argument('--no-sandbox')  # 避免權限問題
-    options.add_argument('--disable-dev-shm-usage')  # 避免 /dev/shm 空間不足
-    options.add_argument('--disable-notifications')
-    options.add_argument('--disable-infobars')
-    options.add_argument('--disable-extensions')
-    options.add_argument('--user-data-dir=/tmp/chrome-user-data')  # 避免使用預設 user 資料夾
+    options.add_argument('--headless')               # 無視覺介面，才能跑在 Docker 裡
+    options.add_argument('--no-sandbox')             # 避免權限問題
+    options.add_argument('--disable-dev-shm-usage')  # 避免使用 /dev/shm（共享記憶體）可能導致空間不足造成的錯誤
+    options.add_argument('--disable-notifications')  # 停用桌面通知（如網站的推播通知）
+    options.add_argument('--disable-infobars')       # 移除瀏覽器畫面上的「Chrome 受到自動化軟體控制」提示
+    options.add_argument('--disable-extensions')     # 停用所有外掛與擴充功能，以提升穩定性與執行速度
     options.add_experimental_option("prefs", {
         "profile.managed_default_content_settings.javascript": 2  # 可選擇禁用 JS
     })
